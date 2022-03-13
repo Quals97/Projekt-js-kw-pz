21ce675541d9a5c70df5140c3b973651a6d4543b
kamilwin21 <65176862+kamilwin21@users.noreply.github.com>
2022-03-13 21:04:26 +0100

    First commit

---
+++ app/src/main/java/com/example/mathapp/authentication/LoginActivity.kt
@@ -0,0 +1,157 @@
+package com.example.mathapp.authentication
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Toast
+import com.example.mathapp.MainActivity
+import com.example.mathapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+
+import com.google.firebase.database.ktx.database
+import kotlinx.android.synthetic.main.activity_login.*
+import org.w3c.dom.Text
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+        user_email.addTextChangedListener(emailWatcher)
+
+        login_button.setOnClickListener(loginListener)
+
+        registration.setOnClickListener(registrationListener)
+
+
+    }
+
+
+
+    private val emailWatcher: TextWatcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+
+            val checkBoolean: Boolean = checkInputUserEmail(user_email.text.toString())
+            if (checkBoolean)
+            {
+                checkUserEmail.setTextColor(Color.GREEN)
+                checkUserEmail.text = "Success"
+            }else if(!checkBoolean)
+            {
+                if (user_email.text.toString().isEmpty())
+                {
+                    checkUserEmail.text = ""
+                }else{
+                    checkUserEmail.setTextColor(Color.RED)
+                    checkUserEmail.text = "Wrong data format (example213@gmail.com) without special charakters."
+                }
+
+            }
+        }
+
+
+    }
+
+    private val passwordWatcher: TextWatcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            val checkBoolean: Boolean = checkInputUserPassword(user_password.text.toString())
+            if(checkBoolean)
+            {
+                checkUserPassword.setTextColor(Color.GREEN)
+                checkUserPassword.text = "Success"
+            }else if(!checkBoolean)
+            {
+                checkUserPassword.setTextColor(Color.RED)
+                checkUserPassword.text = ""
+            }
+        }
+
+
+    }
+
+    private val registrationListener: View.OnClickListener = View.OnClickListener {
+        val intent: Intent = Intent(applicationContext, RegistrationActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    private val loginListener = View.OnClickListener {
+//        val email = "kamilwin21@gmail.com"
+//        val password = "12345678"
+        val email: String = user_email.text.toString()
+        val password: String = user_password.text.toString()
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(){ task ->
+                if(task.isSuccessful){
+                    val database = Firebase.database
+                    val myRef = database.getReference("message")
+
+                    myRef.setValue("Hello, World!")
+
+                    Toast.makeText(applicationContext,
+                        "Pomyślnie zalogowano",
+                        Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
+                    intent.putExtra("email_id", email)
+                    startActivity(intent)
+                    finish()
+
+                }else{
+                    Toast.makeText(applicationContext,
+                        task.exception!!.message.toString(),
+                        Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+
+    }
+
+    private fun checkInputUserPassword(userPassword: String): Boolean{
+        val pattern: Pattern = Pattern.compile("^(\\w+)([\\p{Punct}][\\w]+)\$")
+        val matcher: Matcher = pattern.matcher(userPassword)
+
+        return matcher.matches()
+    }
+    private fun checkInputUserEmail(userEmail:String):Boolean{
+        val pattern: Pattern = Pattern.compile("^(\\w+)([\\-.'][\\w]+)*@(\\w){1,10}\\.([A-Za-z]){2,6}\$")
+        val matcher: Matcher = pattern.matcher(userEmail)
+
+        return matcher.matches()
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/mathapp/authentication/RegistrationActivity.kt
@@ -0,0 +1,220 @@
+package com.example.mathapp.authentication
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.example.mathapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.registration_activity.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registration_activity)
+
+
+        //Nasłuchiwanie wprowadzania emaila przez użytkownika
+        user_email.addTextChangedListener(emailWatcher)
+        //Nasłuchiwanie wprowadzania hasła przez użytkownika
+        user_password.addTextChangedListener(passwordWatcher)
+        //Nasłuchiwanie wprowadzania imienia przez użytkownika
+        user_first_name.addTextChangedListener(firstNameWatcher)
+
+        //Nasłuchiwanie wciśnięcia przycisku 'Zarejestruj'
+        registration_button.setOnClickListener(registrationListener)
+
+
+
+
+
+    }
+
+    private val passwordWatcher: TextWatcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+            val checkBoolean: Boolean = checkUserInputPassword(user_password.text.toString())
+            if(checkBoolean)
+            {
+                checkInputUserPassword.setTextColor(Color.GREEN)
+                checkInputUserPassword.text = "Success"
+            }else if(!checkBoolean){
+                if(user_password.text.toString().isEmpty())
+                {
+                    checkInputUserPassword.text = ""
+                }else{
+                    checkInputUserPassword.setTextColor(Color.RED)
+                    checkInputUserPassword.text = "The password should have between 12 and 64 charakters with special charakters like [',;/.?\\{}] and numbers between 0 and 9"
+                }
+
+
+            }
+        }
+
+
+    }
+
+    private val firstNameWatcher: TextWatcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            val checkBoolean: Boolean = checkUserInputFirtName(user_first_name.text.toString())
+                if (checkBoolean)
+                {
+                    checkInputUserFirstName.setTextColor(Color.GREEN)
+                    checkInputUserFirstName.text = "Success"
+                }else if(!checkBoolean){
+                    if(user_first_name.text.toString().isEmpty())
+                    {
+                        checkInputUserFirstName.text = ""
+                    }else{
+                        checkInputUserFirstName.setTextColor(Color.RED)
+                        checkInputUserFirstName.text = "The name should start with Upper-case without special charakters"
+                    }
+
+                }
+        }
+
+
+    }
+
+    private val emailWatcher: TextWatcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            val checkBoolean: Boolean = checkUserInputEmail(user_email.text.toString())
+
+            if(checkBoolean)
+            {
+                checkInputUserData.setTextColor(Color.GREEN)
+                checkInputUserData.text = "Success"
+            }else if(!checkBoolean){
+                if (user_email.text.toString().isEmpty())
+                {
+                    checkInputUserData.text = ""
+                }else{
+                    checkInputUserData.setTextColor(Color.RED)
+                    checkInputUserData.text = "Wrong data format (example213@gmail.com) without special charakters."
+                }
+
+
+            }
+        }
+
+    }
+
+    private val registrationListener: View.OnClickListener = View.OnClickListener {
+
+        val email: String = user_email.text.toString()
+        val password: String = user_password.text.toString()
+
+
+        if (checkUserInputEmail(user_email.text.toString()) && checkUserInputPassword(user_password.text.toString()) && checkUserInputFirtName(user_first_name.text.toString())
+        ){
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(){
+                        task ->
+                    if (task.isSuccessful)
+                    {
+
+
+                        //val firebaseUser: FirebaseUser = task.result!!.user!!
+
+
+
+                        Toast.makeText(applicationContext, "Pomyślnie zarejestrowano.", Toast.LENGTH_LONG).show()
+                        val intent = Intent(applicationContext, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+
+
+                    }else{
+                        Toast.makeText(applicationContext,
+                            task.exception!!.message.toString(),
+                            Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+
+        }else{
+            Toast.makeText(applicationContext, "Błędny format wprowadzonych danych", Toast.LENGTH_LONG).show()
+        }
+
+
+
+
+    }
+
+    private fun checkUserInputFirtName(firstName:String): Boolean
+    {
+        val pattern: Pattern = Pattern.compile("^[A-Z][a-z]+\$")
+        val matcher: Matcher = pattern.matcher(firstName)
+
+        return matcher.matches()
+    }
+    private fun checkUserInputEmail(email:String): Boolean
+    {
+       // val pattern: Pattern = Pattern.compile("^(\\w+)([\\.\\-]{1}[\\w+])*@([a-z0-9]+)\\.([a-z]{1,3})\$")
+        val pattern: Pattern = Pattern.compile("^(\\w+)([\\-.'][\\w]+)*@(\\w){1,10}\\.([A-Za-z]){2,6}\$")
+        val matcher: Matcher = pattern.matcher(email)
+
+        return matcher.matches()
+    }
+    private fun checkUserInputPassword(password:String): Boolean
+    {
+        //val pattern: Pattern = Pattern.compile("^(?=.*[a-z])(?=.*[0-9])(?=.*[\\p{Punct}[^\\s]]).{12,64}\$")
+        //val pattern: Pattern = Pattern.compile("^(?=.*[a-z])(?=.*[0-9])(?=.*[\\p{Punct}@\$%&#])(?=\\S+\$).{12,64}\$")
+
+        var check: Boolean = false
+        if(password.length in 12..64)
+        {
+            val pattern: Pattern = Pattern.compile("^(\\w+)([\\p{Punct}][\\w]+)\$")
+            val matcher: Matcher = pattern.matcher(password)
+            if (matcher.matches())
+            {
+                check=true
+            }
+        }else{
+            check = false
+        }
+
+        //val pattern: Pattern = Pattern.compile("^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.[@#\$%^&+=])(?=\\S+\$).{8,}\$")
+
+
+
+        //return matcher.matches()
+        return check
+    }
+
+
+}
